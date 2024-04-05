c9f120476aa7647d56a5c8fdd9fe6ec270aa22c4
Harshal Gidh <harshalgidh28102@gmail.com>
2024-04-05 12:02:59 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Harshal Gidh',
+    author_email= 'harshalgidh28102@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
